35e113e40af422f4308d67f04fdb1f6ea0d4633d
amoxley <alex.moxley.dev@gmail.com>
2025-06-15 18:02:13 -0700

    add enemy movement

--- scripts/EnemyPossum/enemyPossum.gd
+++ scripts/EnemyPossum/enemyPossum.gd
@@ -2,7 +2,13 @@ class_name EnemyPossum
 extends CharacterBody2D
 
 @export var player: Player
+@export var enemyPossumMoveState: EnemyPossumMoveState
+@export var move_speed := 20
+
 var CurrentState: EnemyPossumState
+
+func _ready() -> void:
+	change_state(enemyPossumMoveState)
 
 func change_state(new_state: EnemyPossumState):
 	CurrentState = new_state
--- scripts/EnemyPossum/state-machine/EnemyPossumMoveState.gd
+++ scripts/EnemyPossum/state-machine/EnemyPossumMoveState.gd
@@ -1,8 +1,23 @@
 class_name EnemyPossumMoveState
 extends EnemyPossumState
 
+@export var animation: AnimatedSprite2D
+
 func enter_state(enemy_possum_node: EnemyPossum):
 	super(enemy_possum_node)
+	animation.play('walk')
 	
 func handle_process(delta: float):
-	pass
+	var player_position = enemy_possum.player.position
+	var direction = (player_position - enemy_possum.position).normalized();
+
+	enemy_possum.velocity = direction * enemy_possum.move_speed * delta
+	var collision = enemy_possum.move_and_collide(enemy_possum.velocity)
+	if not collision:
+		if direction.x > 0:
+			animation.flip_h = true
+		else:
+			animation.flip_h = false
+
+func exit_state():
+	animation.pause()
