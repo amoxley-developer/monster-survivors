7fba4dd8defcf3f87f431d4bdd550288de23fb8d
amoxley <alex.moxley.dev@gmail.com>
2025-06-25 20:55:25 -0700

    create the take damage function

--- scripts/Player/player.gd
+++ scripts/Player/player.gd
@@ -19,7 +19,7 @@ const CardinalDirections: Dictionary[String, String] =  {
 @export var total_dash_distance := 100
 @export var dash_delay_length := 0.1
 @export var can_player_dash := true
-# health needs to be float because delta is a float, otherwise it will round
+# health needs to be float because delta is a float, otherwise it will round down, causing health to decrease rapidly
 @export var health := 100.0
 
 var CurrentState: PlayerState
@@ -27,23 +27,25 @@ var cardinal_direction: String = CardinalDirections.get("Down")
 
 func _ready():
 	change_state(IdleState)
-	
+
 func change_state(new_state: PlayerState):
 	CurrentState = new_state
 	if CurrentState:
 		CurrentState.enter_state(self)
 		
 func _process(delta: float) -> void: 
-	health -= 20 * delta
-	if health <= 0 and CurrentState != DeadState:
-		change_state(DeadState)
-
+	take_damage(20*delta)
 	if CurrentState:
 		CurrentState.handle_process(delta)
-	
 
 func start_dash_delay_timer():
 	get_tree().create_timer(dash_delay_length).timeout.connect(player_can_dash)
 	
 func player_can_dash():
 	can_player_dash = true
+
+func take_damage(amount: float):
+	if health <= 0 and CurrentState != DeadState:
+		change_state(DeadState)
+	else:
+		health -= amount
--- scripts/Player/state-machine/DeadState.gd
+++ scripts/Player/state-machine/DeadState.gd
@@ -2,25 +2,19 @@ class_name PlayerDeadState
 extends PlayerState
 
 # 1 second delay before the death animation plays
-var death_animation_timer := 1.0
-var animation_played := false
+var death_animation_delay := 1.0
+var has_animation_played := false
 
-func enter_state(player_node: Player):
+func enter_state(player_node: Player) -> void:
 	super(player_node)
-	handle_animation()
-	
+	player.PlayerAnimation.play('dead', 0)
 
-func handle_process(delta: float):
-	if death_animation_timer > 0:
-		death_animation_timer -= 1*delta
-	elif not animation_played:
+func handle_process(delta: float) -> void: 
+	if death_animation_delay > 0:
+		death_animation_delay -= delta
+	elif not has_animation_played:
+		has_animation_played = true
 		handle_animation()
-		animation_played = true
 
-func handle_animation():
-	if death_animation_timer > 0:
-		player.PlayerAnimation.play('dead', 0)
-		return
-	
+func handle_animation() -> void:
 	player.PlayerAnimation.play('dead')
-	return
