077f8137827ad97b9b4903776fcb9ef14632672f
amoxley <alex.moxley.dev@gmail.com>
2025-07-13 12:20:12 -0700

    add possum contact damage

--- scripts/Player/player.gd
+++ scripts/Player/player.gd
@@ -19,16 +19,25 @@ const CardinalDirections: Dictionary[String, String] =  {
 @export var walk_speed := 25
 @export var total_dash_distance := 100
 @export var dash_delay_length := 0.1
-@export var can_player_dash := true
 # health needs to be float because delta is a float, otherwise it will round down, causing health to decrease rapidly
 @export var health := 100.0
+@export var damage_delay_timer: Timer
+@export var damage_delay_length := .5
 
+var can_take_damage := true
+var can_player_dash := true
 var CurrentState: PlayerState
 var cardinal_direction: String = CardinalDirections.get("Down")
 
 func _ready():
 	change_state(IdleState)
 	HealthBarScene.init_health(health, Color.FOREST_GREEN)
+	set_damage_delay_timer()
+
+func set_damage_delay_timer():
+	damage_delay_timer.one_shot = true
+	damage_delay_timer.wait_time = damage_delay_length
+	damage_delay_timer.timeout.connect(player_can_take_damage)
 
 func change_state(new_state: PlayerState):
 	CurrentState = new_state
@@ -41,13 +50,22 @@ func _process(delta: float) -> void:
 
 func start_dash_delay_timer():
 	get_tree().create_timer(dash_delay_length).timeout.connect(player_can_dash)
+
+func player_can_take_damage():
+	can_take_damage = true
 	
 func player_can_dash():
 	can_player_dash = true
 
 func take_damage(amount: float):
-	if health <= 0 and CurrentState != DeadState:
+	if health <= 0 and CurrentState is not PlayerDeadState:
 		change_state(DeadState)
-	elif health > 0:
+	elif (
+		health > 0 
+		and CurrentState is not PlayerDashState 
+		and can_take_damage
+	):
+		can_take_damage = false
+		damage_delay_timer.start()
 		health -= amount
 		HealthBarScene._set_health(health)
