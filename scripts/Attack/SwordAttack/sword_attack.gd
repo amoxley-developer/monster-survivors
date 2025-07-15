afc6274fe27a3343d2a033c4a06a3cd65c6df3f1
amoxley <alex.moxley.dev@gmail.com>
2025-07-14 20:20:45 -0700

    update sword attack script

---
+++ scripts/Attack/SwordAttack/sword_attack.gd
@@ -0,0 +1,10 @@
+class_name SwordAttack
+extends Area2D
+
+func _ready():
+    # initialize the delay timer
+        # delay timer should repeat
+    # hide animation and collision box
+    pass
+
+# on delay timer completion the animation and collision start
